d761d5c3ba1251172e4a56ca7b948842f013a60b
Patrick Divos <divos.patrick@gmail.com>
2020-07-30 02:24:48 +0200

    Update __init__.py
    added the needed code to scrape the links hidden in the tags

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def scraper():
+    link = 'none'
     list0 = []
     list1 = []
     list2 = []
@@ -11,15 +12,15 @@ def scraper():
     driver.get('https://www.ip-finder.me/ip-full-list/')
     content = driver.page_source
     soup = BeautifulSoup(content)
-    #for a in soup.findAll('a', href=True, attrs={'class': 'topIpsDiv'}):
     for a in soup.findAll('div', attrs={'class': 'topIP'}):
         address = a.find('div', attrs={'class': 'ip'})
         block_count = a.find('div', attrs={'class': 'count text-blue'})
-        link = a.find('a', href=True, attrs={'class': 'ip'})
+        print(a)
+        for b in a.find_all('a', href=True):
+            link = b['href']
         list0.append(address.text)
         list1.append(block_count.text)
         list2.append(link)
-        #TODO: Scrape links
     df = pd.DataFrame({'in_address': list0, 'block_count': list1, 'link_to_reason': list2})
     df.to_csv('critical_ip_addresses.csv', index=False, encoding='utf-8')
 
