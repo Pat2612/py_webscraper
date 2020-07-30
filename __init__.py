e54a85d4406b1c9206f424aa2762e975c2675955
Patrick Divos <divos.patrick@gmail.com>
2020-07-30 02:38:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,20 +9,21 @@ def scraper():
     list1 = []
     list2 = []
     driver = webdriver.Chrome()
-    driver.get('https://www.ip-finder.me/ip-full-list/')
+    driver.get('link to website')
     content = driver.page_source
     soup = BeautifulSoup(content)
-    for a in soup.findAll('div', attrs={'class': 'topIP'}):
-        address = a.find('div', attrs={'class': 'ip'})
-        block_count = a.find('div', attrs={'class': 'count text-blue'})
+    for a in soup.findAll('div', attrs={'class': 'class-name'}):
+        # you can use more than one class-name
+        address = a.find('div', attrs={'class': 'class-name class-name'})
+        block_count = a.find('div', attrs={'class': 'class-name'})
         print(a)
         for b in a.find_all('a', href=True):
             link = b['href']
         list0.append(address.text)
         list1.append(block_count.text)
         list2.append(link)
-    df = pd.DataFrame({'in_address': list0, 'block_count': list1, 'link_to_reason': list2})
-    df.to_csv('critical_ip_addresses.csv', index=False, encoding='utf-8')
+    df = pd.DataFrame({'first_field': list0, 'second_field': list1, 'third_field': list2})
+    df.to_csv('filename.csv', index=False, encoding='utf-8')
 
 
 if __name__ == '__main__':
